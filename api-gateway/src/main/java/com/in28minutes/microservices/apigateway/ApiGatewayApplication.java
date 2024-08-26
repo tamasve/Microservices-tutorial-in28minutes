bb3c317c6487afc0f145606e50c5b108b7e0fac8
tamasve <145750552+tamasve@users.noreply.github.com>
2024-08-27 01:10:06 +0200

    comments, theory

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,8 @@ package com.in28minutes.microservices.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// The entire Authentication logic can be implemented in API gateway
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -23,17 +23,17 @@ public class ApiGatewayConfiguration {
 //                                                            .addRequestParameter("MyParam", "MyValue"))         // my parameter into the "args"
 //                                .uri("http://httpbin.org:80");     // redirect to this URI if the request is /get
         return builder.routes()
-//                .route(routeFunction)
+//                .route(routeFunction)         // this would be if we used the above Function definition, but instead we use directly lambdas
                 .route(
                         p -> p.path("/get")     // the route to redirect - "match" = "predicate"
-                                .filters( f -> f.addRequestHeader("MyHeader", "MyURI")      // my parameter into the "headers" - filter = what needs to be done once a request is matched
-                                        .addRequestParameter("MyParam", "MyValue"))         // my parameter into the "args"
+                                .filters( f -> f.addRequestHeader("CustomHeaderParameter", "SpecURI")      // my parameter into the "headers" - filter = what needs to be done once a request is matched
+                                        .addRequestParameter("Parameter", "ParValue"))         // my parameter into the "args" data part
                                 .uri("http://httpbin.org:80")     // redirect to this URI if the request is "/get"
                 )
                 .route(
                         p -> p.path("/currency-exchange/**")
-                                        .uri("lb://currency-exchange")      // lb: load balancer with simple url, no duplication (app name + url)
-                )
+                                        .uri("lb://currency-exchange")      // lb: load balancer, this invokes the url from the Eureka Server = no duplication (app name + url) + the port always is the API gateway's port: 8765
+                )                                               // http://localhost:8765/currency-exchange/from/USD/to/INR
                 .route(
                         p -> p.path("/currency-conversion/**")
                                 .uri("lb://currency-conversion")      // lb: load balancer
